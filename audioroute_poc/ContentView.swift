2e5c7b07f886198fc6d1f6e2a2cd72cefa5b377f
Miguel de Sousa <mfsousa@poli.ufrj.br>
2021-07-20 10:52:48 -0300

    Initial Commit

---
+++ audioroute_poc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  audioroute_poc
+//
+//  Created by Miguel Fernandes de Sousa on 20/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ audioroute_poc/audioroute_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  audioroute_pocApp.swift
+//  audioroute_poc
+//
+//  Created by Miguel Fernandes de Sousa on 20/07/21.
+//
+
+import SwiftUI
+
+@main
+struct audioroute_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
